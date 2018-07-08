8da9c7a159eeed05e1bc5a1776dbfd469c6d22c6
Hector Moreno <hectormov@gmail.com>
2018-07-07 23:06:49 -0500

    Feat: Changed Search to functional component

--- src/Search.js
+++ src/Search.js
@@ -1,40 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Search extends Component {
-    
-    render() {
-        const {locations, filteredLocations, selectedLocation, selectLocation, queryUpdate} = this.props; 
-        let locs = filteredLocations === [] ? locations : filteredLocations;   
-        return (
-            <div className='search-and-list'>
-                <div>
-                    <input type="text" 
-                        className='search-box' 
-                        placeholder='Restaurant Name' 
-                        aria-label='Search Restaurant'
-                        onChange={event => queryUpdate(event.target.value)}/>
-                </div>
-                <ul>
-                    {
-                        locs.map((location, index) => {
-                            const isSelected = (location.id === selectedLocation.id ? 'row-selected' : '');
-                            return (
-                                <li 
-                                    key={index} 
-                                    onClick={ () => selectLocation(location)}
-                                    onKeyDown={(event) => event.keyCode !== 13 || selectLocation(location)}
-                                    className={isSelected}
-                                    tabIndex={0}
-                                >
-                                    <span>{location.name}</span>
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
+const Search = (props) => {
+    const {locations, filteredLocations, selectedLocation, selectLocation, queryUpdate} = props; 
+    let locs = filteredLocations === [] ? locations : filteredLocations;   
+    return (
+        <div className='search-and-list'>
+            <div>
+                <input type="text" 
+                    className='search-box' 
+                    placeholder='Restaurant Name' 
+                    aria-label='Search Restaurant'
+                    onChange={event => queryUpdate(event.target.value)}/>
             </div>
-        );
-    }
+            <ul>
+                {
+                    locs.map((location, index) => {
+                        const isSelected = (location.id === selectedLocation.id ? 'row-selected' : '');
+                        return (
+                            <li 
+                                key={index} 
+                                onClick={ () => selectLocation(location)}
+                                onKeyDown={(event) => event.keyCode !== 13 || selectLocation(location)}
+                                className={isSelected}
+                                tabIndex={0}
+                            >
+                                <span>{location.name}</span>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    );
 }
 
 export default Search;
